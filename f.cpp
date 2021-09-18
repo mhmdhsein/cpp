689e57cd7231c307a9bee382c883ac0dd5ccc10c
mhmdhseinltifi <89693344+mhmdhsein@users.noreply.github.com>
2021-09-18 18:34:52 +0430

    Create f.cpp

---
+++ f.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a1[2][3]={{2,3,4},{3,5,6}};
+int a2[3];
+for(int f=0;f<2;f++)
+    for(int m=0;m<3;m++)
+    if(f==0)
+    a2[m]=a1[f][m];
+for(int h=0;h<3;h++)
+    cout<<a2[h];
+    return 0;
+}
+© 2021 GitHub, Inc.
